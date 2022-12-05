4dd1c120cffb1791c71be7abb48136a22d1e81c7
Fsh-98 <sharukh75hasan@gmail.com>
2022-12-05 17:18:36 +0600

    added view for dashboard for logged in user.

---
+++ src/resources/views/Romatoo/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Dashboard</div>
+	<div class="card-body">
+		
+		Hello world
+	</div>
+</div>
+
+@endsection('content')
