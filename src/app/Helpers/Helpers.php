1118b75e85e33a1183d312749d25f187d8306e7a
Fsh-98 <sharukh75hasan@gmail.com>
2022-12-06 22:35:28 +0600

    add Helper class for custom helper methods.

---
+++ src/app/Helpers/Helpers.php
@@ -0,0 +1,27 @@
+<?php
+  
+use Carbon\Carbon;
+  
+/**
+ * Write code on Method
+ *
+ * @return response()
+ */
+if (! function_exists('convertYmdToMdy')) {
+    function convertYmdToMdy($date)
+    {
+        return Carbon::createFromFormat('Y-m-d', $date)->format('m-d-Y');
+    }
+}
+  
+/**
+ * Write code on Method
+ *
+ * @return response()
+ */
+if (! function_exists('convertMdyToYmd')) {
+    function convertMdyToYmd($date)
+    {
+        return Carbon::createFromFormat('m-d-Y', $date)->format('Y-m-d');
+    }
+}
