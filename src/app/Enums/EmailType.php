2dbe7368cc36f30d3c489bf1a22100938d14e6bb
Fsh-98 <sharukh75hasan@gmail.com>
2022-12-07 14:14:33 +0600

    updated enums of email type

--- src/app/Enums/EmailType.php
+++ src/app/Enums/EmailType.php
@@ -8,4 +8,18 @@ interface EmailType
     const SOCIAL = 2;
     const PROMOTIONAL = 3;
     const FORUM = 4;
+
+    const EMAIL_TYPES = [
+        self::PRIMARY,
+        self::SOCIAL,
+        self::PROMOTIONAL,
+        self::FORUM,
+    ];
+    const EMAIL_TYPES_WITH_LABEL = [
+        self::PRIMARY => 'Primary',
+        self::SOCIAL => 'Social',
+        self::PROMOTIONAL => 'Promotinal',
+        self::FORUM => 'Forum',
+    ];
+
 }
