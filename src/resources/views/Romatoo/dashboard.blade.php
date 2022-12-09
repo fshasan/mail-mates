f5e9afa2c67e9c25f176b98f6fa41894719aa608
Fsh-98 <sharukh75hasan@gmail.com>
2022-12-09 17:31:43 +0600

    show user inbox in dahboard.

--- src/resources/views/Romatoo/dashboard.blade.php
+++ src/resources/views/Romatoo/dashboard.blade.php
@@ -19,7 +19,7 @@
 
 	<div class="card-body mt-4">
 		<h5>You have a total of {{$emails->count()}} messages.</h5>
-		@include('Romatoo.components.sent')
+		@include('Romatoo.components.inbox')
 	</div>
 
 </div>
